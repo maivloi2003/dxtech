d093a1f3993c941f9e4e93346a2f012d4a06a134
vanloi <maivloi2003@gmail.com>
2025-04-28 21:49:56 +0700

    responsive mobile

--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,7 +1,10 @@
 import classNames from "classnames/bind";
+import { useState } from "react";
 import styles from './NavBar.module.scss'
 import Logo from "../Logo";
 import Button from "../Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const cx = classNames.bind(styles)
 
@@ -24,6 +27,15 @@ const categoryNav = [
 ]
 
 function NavBar() {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false);
+    };
     return (
         <div className={cx('wrapper')}>
             <Logo color='black' />
@@ -34,8 +46,33 @@ function NavBar() {
                         <li className={cx('nav-item')} key={index}>{item.title}</li>
                     ))}
                 </ul>
-                <Button outline>Request a quote</Button>
+                <Button className={cx('btn-quote')} outline>Request a quote</Button>
+
             </div>
+            <FontAwesomeIcon className={cx('icon-menu')} icon={faBars} onClick={toggleMenu} />
+            {isOpen && (
+                <div className={cx('overlay')}>
+                    <div className={cx('overlay-content')}>
+                        <FontAwesomeIcon
+                            icon={faTimes}
+                            className={cx('close-icon')}
+                            onClick={closeMenu}
+                        />
+                        <ul className={cx('overlay-nav-list')}>
+                            {categoryNav.map((item, index) => (
+                                <li
+                                    className={cx('overlay-nav-item')}
+                                    key={index}
+                                    onClick={closeMenu}
+                                >
+                                    {item.title}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                    <div className={cx('overlay-background')} onClick={closeMenu}></div>
+                </div>
+            )}
         </div>
     );
 }
